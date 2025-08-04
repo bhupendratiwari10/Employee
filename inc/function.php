a872a4a16418e8a7f1addcba3e5446535cff9bdf
Bhoopendra Tiwari <bhoopendra.tiwari@appinventiv.com>
2025-08-04 12:31:33 +0530

    improved

--- inc/function.php
+++ inc/function.php
@@ -14,7 +14,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
 ini_set('error_log', '/path/to/error.log');
-include_once('../constant.php');
+define('ALLOW_CONFIG_ACCESS', true);
+
+// Include configuration
+require_once(dirname(__DIR__) . '/constant.php');
 /**
  * Database connection
  */
